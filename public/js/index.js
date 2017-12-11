ce6fd0a379fdf7055882966cae93944aed4c2b8e
MatthewAlviz <wehttamzivla@gmail.com>
2017-12-11 12:12:59 +0800

    Added Log-in with special effects

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,22 @@
 $(document).ready(function(){
   $("#Login").on("click", function() {
     //$('.main').fadeTo("slow",0.5);
+    $('.form').removeClass("fadeOut");
     $('.main').removeClass("deblur");
     $('.main').addClass("blur");
+    $('.form').css("z-index", "2");
+    $('.form').addClass("fadeInUp animated");
   });
 
   $('.main').on("click", function(){
         $('.main').addClass("deblur");
+        $('.form').addClass("fadeOut animated").removeClass("fadeInUp");
+
+        /*.delay(600).queue( function(next){
+          $(this).css("z-index", "0");
+          $(this).removeClass("fadeInUp animated");
+          $(this).removeClass("fadeOut");
+        });
+        */
   });
 });
