3c69cf8391e389d53ca72893b919b63c1641c6cf
MatthewAlviz <wehttamzivla@gmail.com>
2018-01-04 20:07:39 +0800

    Added changing of book view from coverflow to flat and vice versa.

--- public/js/burger.js
+++ public/js/burger.js
@@ -1,7 +1,14 @@
 var menuBoolean = false;
+
+//if ($('#cover').attr("active") == "true"){
 var prev = $("#flip").attr("style");
 var prevTransform = prev.split("(");
 var update = prevTransform[1].split(")");
+//}else{
+/*var prev1 = $("#flip1").attr("style");
+var prevTransform1 = prev.split("(");
+var update1 = prevTransform1[1].split(")");*/
+//}
 
 $('.nav-wrapper').click(function(ev) {
 			$('#nav-trigger-anim').toggleClass('active');
@@ -9,14 +16,22 @@ $('.nav-wrapper').click(function(ev) {
 						$('#sideMenu').removeClass("fadeOutLeft animated");
 						$('#sideMenu').css("display", "");
 						$('#sideMenu').addClass("fadeInLeft animated");
-						$('.flipster__container').css("transform", "translateX(20%)");
+						if($('#cover').attr("active") == "true"){
+							$('.flipster__container').css("transform", "translateX(20%)");
+						} else{
+							$('.flipster__container').css("transform", "translateX(0%)");
+						}
 						menuBoolean = true;
 			} else {
 						$('#sideMenu').removeClass("fadeInLeft animated").addClass("fadeOutLeft animated").delay(400).queue(function (next) {
     $(this).css('display', 'none');
     next();
   });
-						$('.flipster__container').css("transform", "translateX("+update[0]+")");
+						//if ($('#cover').attr("active") == "true"){
+							$('.flipster__container').css("transform", "translateX("+update[0]+")");
+						/*}else{
+							$('.flipster__container').css("transform", "translateX("+update1[0]+")");
+						}*/
 						menuBoolean = false;
 			}
 });
