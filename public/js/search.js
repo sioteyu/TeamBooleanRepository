bfca7a1666ca235a239667aa6e5cd12e1db968e3
MatthewAlviz <wehttamzivla@gmail.com>
2018-01-11 15:35:20 +0800

    Added Upload Form via clicking upload books from scroll down menu by clicking the profile picture.

--- public/js/search.js
+++ public/js/search.js
@@ -1,13 +1,33 @@
 $(document).ready(function(){
   $(".searchBut").on("click", function() {
-    $('.site-search').addClass('open');
+    $('#site1').addClass('open');
   });
 
-  jQuery('.site-search').click(function(){
-    jQuery('.site-search').removeClass('open');
+  $("#close1").on("click", function() {
+    $('#site1').removeClass('open');
+  });
+
+  $("#upload").on("click", function() {
+    $('#site2').addClass('open');
+  });
+
+  $("#close2").on("click", function() {
+    $('#site2').removeClass('open');
+  });
+
+  /*jQuery('#site1').click(function(){
+    jQuery('#site1').removeClass('open');
   }).children().click(function(e) {
     return false;
   });
 
+  $("#upload").on("click", function() {
+    $('#site2').addClass('open');
+  });
 
+  jQuery('#site2').click(function(){
+    jQuery('#site2').removeClass('open');
+  }).children().click(function(e) {
+    return false;
+  });*/
 });
