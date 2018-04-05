8c5bb781ab3e5d67d9775db02889963d3e5a5a2f
MatthewAlviz <wehttamzivla@gmail.com>
2018-04-05 09:00:33 +0800

    Added Share

--- js/bookPreview/log-in.js
+++ js/bookPreview/log-in.js
@@ -4,13 +4,8 @@ $('.log-in').click(function(){
   $('#hey').css('overflow', 'hidden');
   $('.register-form').css('display', 'none');
   $('.login-form').css('display', 'unset');
+  $('.share-form').css('display', 'none');
   $('.form2 h1').text('Log-in');
-});
-
-$('.here').click(function(){
-  $('.here').css('display', 'none');
-  $('.login-page').css('display', 'none');
-  $('#hey').css('overflow', 'auto');
 });
 
 $('.sign-up').click(function(){
@@ -19,7 +14,34 @@ $('.sign-up').click(function(){
   $('#hey').css('overflow', 'hidden');
   $('.register-form').css('display', 'unset');
   $('.login-form').css('display', 'none');
+  $('.share-form').css('display', 'none');
   $('.form2 h1').text('Sign-up');
+});
+
+$('#share-this').click(function(){
+  $('.here').css('display', 'unset');
+  $('.login-page').css('display', 'unset');
+  $('#hey').css('overflow', 'hidden');
+  $('.register-form').css('display', 'none');
+  $('.login-form').css('display', 'none');
+  $('.share-form').css('display', 'unset');
+  $('.form2 h1').text('Share via');
+});
+
+$('#share-here').click(function(){
+  $('.here').css('display', 'unset');
+  $('.login-page').css('display', 'unset');
+  $('#hey').css('overflow', 'hidden');
+  $('.register-form').css('display', 'none');
+  $('.login-form').css('display', 'none');
+  $('.share-form').css('display', 'unset');
+  $('.form2 h1').text('Share via');
+});
+
+$('.here').click(function(){
+  $('.here').css('display', 'none');
+  $('.login-page').css('display', 'none');
+  $('#hey').css('overflow', 'auto');
 });
 
 $('.mssg1').click(function(){
