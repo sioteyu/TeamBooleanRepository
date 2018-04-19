571adb7acbbef444dcb1a7dd4047f3981c8080ce
MatthewAlviz <wehttamzivla@gmail.com>
2018-04-19 14:03:46 +0800

    Nav quick fix on book result

--- public/js/landingpage/extra.js
+++ public/js/landingpage/extra.js
@@ -1,1 +1,4 @@
-setTimeout(function(){$('.current-selection').html("<i class='fa fa-book'></i>   Book"); $('.chosen-single').attr('style', "font-family: 'Circular Std Book'")}, 1);
+setTimeout(function(){
+  $('.current-selection').html("<i class='fa fa-book'></i>   Book");
+  $('.chosen-single').attr('style', "font-family: 'Circular Std Book'");
+}, 100);
